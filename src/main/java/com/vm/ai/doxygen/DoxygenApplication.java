14414030c45cb47afbdae43924b4fd021d50d677
Gurpreet Singh <gurpreet.singh1@valuemomentum.com>
2025-04-28 13:56:09 +0530

    Setup of the project structure

---
+++ src/main/java/com/vm/ai/doxygen/DoxygenApplication.java
@@ -0,0 +1,13 @@
+package com.vm.ai.doxygen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoxygenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoxygenApplication.class, args);
+	}
+
+}
